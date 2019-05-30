f909f214563ff205f0194ea4842f935d4caf42e5
Ícaro Jerry <icaro.jerry@gmail.com>
2019-05-30 13:09:13 -0300

    Update Project

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,6 @@
 @section('content')
 <div class="container">
 	<h1 class="title">Welcome to Project Manager</h1>
-	<h2 class="sub-title"></h2>
+	<h2 class="sub-title">manage your project tasks</h2>
 </div>
 @endsection
